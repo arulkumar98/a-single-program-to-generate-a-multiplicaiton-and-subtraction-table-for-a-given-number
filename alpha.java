c072b40081dcf8589e96c37766298f416b8b7f68
Arul Kumar <arulkumar202@gmail.com>
2022-03-20 11:12:40 +0530

    first commit

---
+++ alpha.java
@@ -0,0 +1,19 @@
+class table
+{
+	public static void main(String arg[])
+	{
+		// Given number as n
+		int n = 77;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+
+		for (int j = 0; j <= 10; j++) {
+			System.out.println(n + " - " + j + " = " + (n - j));
+        }
+        
+	}
+}
+
+
